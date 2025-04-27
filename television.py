52f1b4e15a93e6e1bc223ed597ed021229900e4c
Schondo7 <schondo7@gmail.com>
2025-04-27 17:12:49 -0500

    commit to add docs

--- television.py
+++ television.py
@@ -6,19 +6,23 @@ class Television:
     MAX_CHANNEL = 3
 
     def __init__(self) -> None:
+        '''this section gives the default values for the tv'''
         self.__status = False
         self.__muted = False
         self.__volume = 0
         self.__channel = 0
 
     def power(self):
+        '''this section turns the tv on and off'''
         self.__status = not self.__status
 
     def mute(self):
+        '''this will turn the volume all the way down'''
         if self.__status:
             self.__muted = not self.__muted
 
     def channel_up(self):
+       '''this is used to go up in the channels'''
        if self.__status:
             if self.__channel < Television.MAX_CHANNEL:
                self.__channel += 1
@@ -26,6 +30,7 @@ class Television:
                self.__channel = Television.MIN_CHANNEL
 
     def channel_down(self):
+        ''' this is used for going down in channels'''
         if self.__status:
             if self.__channel > Television.MIN_CHANNEL:
                 self.__channel -= 1
@@ -34,6 +39,7 @@ class Television:
 
 
     def volume_up(self):
+        '''to turn the volume up louder'''
         if self.__status:
             self.__muted = False
             if self.__volume < Television.MAX_VOLUME:
@@ -41,6 +47,7 @@ class Television:
 
 
     def volume_down(self):
+        '''to turn the volume down lower'''
         if self.__status:
             self.__muted = False
             if self.__volume > Television.MIN_VOLUME:
